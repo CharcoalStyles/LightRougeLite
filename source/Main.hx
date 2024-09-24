c83a9586bf647f9fddd022f128c9ff80aec127eb
Aaron Styles <aaron@charcoalstyles.com>
2024-09-24 10:53:24 +0000

    Init commit

---
+++ source/Main.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+import states.FlxCstyLogo;
+import states.MenuState;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(0, 0, () ->
+		{
+			new FlxCstyLogo(new PlayState(), true);
+		}));
+	}
+}
